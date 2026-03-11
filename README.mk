4ca66436649ba783e4c9c2ee6119e5207db0b885
Alvaro Madrid Morales <metrasta211@gmail.com>
2026-03-10 18:42:56 -0600

    Agregacion de favicon e index modificación

---
+++ README.mk
@@ -0,0 +1,30 @@
+# Three.js Demo Collection
+
+Proyecto académico que integra múltiples ejemplos de Three.js en una interfaz web moderna y minimalista.
+
+## Demos incluidas
+
+- Geometry / Minecraft
+- Controls / Map
+- Controls / Orbit
+- Controls / Pointer Lock
+
+## Tecnologías utilizadas
+
+- Three.js
+- HTML5
+- CSS3
+- UIverse Components
+
+## Características
+
+- Diseño minimalista
+- Interfaz responsive
+- Navegación mediante cards
+- Implementación modular de demos
+
+## Autor
+
+Álvaro Madrid Morales
+
+Instituto Tecnológico de Pachuca
